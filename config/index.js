850474b1d085a36fa503351b0ad0379af53ac352
guojiongwei <18530633992@163.com>
2020-09-24 17:15:58 +0800

    功能调整：迁移服务器ip地址地址

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 exports.mongodb = {
-  dbUrl: 'mongodb://localhost:27017/test',
+  // dbUrl: 'mongodb://localhost:27017/test',
+  dbUrl: `mongodb://guojiongwei:123456@39.107.107.224:27017/wuyou`,
   dbName: 'wuyou'
 }
